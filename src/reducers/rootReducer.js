3378aab23b283a29d55b1527d3a11b35826ea150
basit <i@basit.me>
2018-08-10 12:39:03 +0100

    back to default

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import {combineReducers} from 'redux';
 import stuff from './stuffReducer';
-import pickupDateTime from './pickupDateTimeReducer';
 
 const rootReducer = combineReducers({
     stuff,
-    pickupDateTime
 });
 
 export default rootReducer;
